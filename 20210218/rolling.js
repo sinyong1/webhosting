5a788a803f16c666da5f1ccd8296a0bd37d445d9
sinyong1 <tls00012@naver.com>
2021-02-24 16:32:52 +0900

    커밋

--- 20210218/rolling.js
+++ 20210218/rolling.js
@@ -5,15 +5,40 @@ var last;
 var interval;
 
 $(document).ready(function(){
-$(this).css("letf",banner_left);
+$("rolling_wrap a").each(function (){
+    $(this).css("letf",banner_left);
 banner_left +=$(this).width()+5;
 $(this).attr("id","contetn"+(++img_cnt));
 });
 
 last=img_cnt;
 startAction();
+
 $(".content").hover(
-    function(){stopAction();},
-    function({startAction(); }) ;
+    function() { stopAction(); },
+    function() {startAction(); });
 });
-https://greatps1215.tistory.com/13
+function startAction()
+{
+interval=setInterval(function (){
+    $(".rolling_wrap a").each(function (){
+        $(this).css("left",$(this).position().left-1);
+    });
+    var firtst_content = $("#content"+first);
+    var last_content =$("#content"+last);
+
+    if(firtst_content.position().left <"-"+$(first_content).width())
+    {
+first_content.css("left",last_content.position().left+last_content.width()+5);
+firtst++;
+lst++;
+if(last > img_cnt){last=1;}
+if(first> img_cnt){fist=1;}
+
+      }
+   },15);
+}
+
+function stopAction(){
+    clearInterval(interval);
+}
